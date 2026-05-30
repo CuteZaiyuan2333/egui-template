eee58ef693bc3abe59b4d2e7e554fa13f41e1138
CuteZaiyuan2333 <your.email@example.com>
2026-05-30 14:47:26 +0800

    empty template for egui desktop applications

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use eframe::egui;
+
+struct Application {
+    // Empty
+}impl Application{
+    fn init() -> Self{
+        Application{
+            // Empty
+        }
+    }
+}
+
+impl eframe::App for Application {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::Panel::top("top-panel").show_inside(ui, |ui|{
+            ui.menu_button("place-holder", |ui|{
+                let _ = ui.button("place-holder1");
+                ui.separator();
+                let _ = ui.button("place-holder2");
+            });
+        });
+        egui::Panel::bottom("btm-panel").show_inside(ui, |ui|{
+            ui.label("place-holder");
+        });
+        egui::Panel::left("left-panel").resizable(true).size_range(100.0..=400.0).show_inside(ui, |ui|{
+            ui.label("place-holder");
+            ui.take_available_space()
+        });
+        egui::Panel::right("right-panel").resizable(true).size_range(100.0..=400.0).show_inside(ui, |ui|{
+            ui.label("place-holder");
+            ui.take_available_space()
+        });
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            ui.label("place-holder");
+        });
+    }
+}
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Application",
+        options,
+        Box::new(|_cc| Ok(Box::new(Application::init()))),
+    )
+}
